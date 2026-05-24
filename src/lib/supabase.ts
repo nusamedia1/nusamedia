d0b58e5bdbbf7366847152a032aa5113e368c6cd
ikhwanpermana <email_github_anda@example.com>
2026-05-24 23:39:17 +0900

    fix: pembetulan interpolasi seo dan url svg detail berita

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Mengakali sistem dengan menaruh string koneksi asli secara langsung
-const supabaseUrl = "https://zmvlvzspjohrewjbtfxc.supabase.co" 
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inptdmx2enNwam9ocmV3amJ0ZnhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwMTM0MTAsImV4cCI6MjA5NDU4OTQxMH0.PtTVq_b4gLonRpNSbPYKncoXlGVS1lKaADy74dbhuSI" 
+const supabaseUrl = "https://gnuxrjwiyiikkyjxgzff.supabase.co/" 
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdudXhyandpeWlpa2t5anhnemZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2MTQyOTQsImV4cCI6MjA5NTE5MDI5NH0.OFjoavfuIebaPiSLSgDyf-rUbfyzfvf9dibRgkmoNDg" 
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
