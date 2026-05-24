069825a188e15db6753562153c5a5064321f6d0f
ikhwanpermana <email_github_anda@example.com>
2026-05-25 00:33:03 +0900

    final

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Mengakali sistem dengan menaruh string koneksi asli secara langsung
-const supabaseUrl = "https://gnuxrjwiyiikkyjxgzff.supabase.co/" 
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdudXhyandpeWlpa2t5anhnemZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2MTQyOTQsImV4cCI6MjA5NTE5MDI5NH0.OFjoavfuIebaPiSLSgDyf-rUbfyzfvf9dibRgkmoNDg" 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+// Memberikan nilai cadangan langsung agar tidak memicu error "supabaseUrl is required"
+export const supabase = createClient(
+  supabaseUrl || "https://gnuxrjwiyiikkyjxgzff.supabase.co", 
+  supabaseKey || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdudXhyandpeWlpa2t5anhnemZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2MTQyOTQsImV4cCI6MjA5NTE5MDI5NH0.OFjoavfuIebaPiSLSgDyf-rUbfyzfvf9dibRgkmoNDg"
+)
