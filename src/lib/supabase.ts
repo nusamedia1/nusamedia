3bac4437700cc777b6bdc50090842bf77279956a
ikhwanpermana <email_github_anda@example.com>
2026-05-25 00:37:13 +0900

    astro

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+// Menggunakan import.meta.env khusus untuk sistem Astro
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL
+const supabaseKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY
 
-// Memberikan nilai cadangan langsung agar tidak memicu error "supabaseUrl is required"
+// Nilai cadangan langsung agar server Astro tidak crash saat memuat halaman
 export const supabase = createClient(
   supabaseUrl || "https://gnuxrjwiyiikkyjxgzff.supabase.co", 
   supabaseKey || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdudXhyandpeWlpa2t5anhnemZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2MTQyOTQsImV4cCI6MjA5NTE5MDI5NH0.OFjoavfuIebaPiSLSgDyf-rUbfyzfvf9dibRgkmoNDg"
