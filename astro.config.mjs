ad3089c04f926303693bc8adbdf9fea672792db9
ikhwanpermana <email_github_anda@example.com>
2026-05-27 17:08:44 +0900

    fix: memperbaiki sintaks terputus di index.astro dan migrasi penuh ke SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: 'server', // 💡 Ubah ke server agar Astro membangun file _worker.js tunggal
+  output: 'hybrid', // 💡 Diubah ke hybrid untuk membongkar bug compiler Vite
   adapter: cloudflare(),
   image: {
     service: {
